744137306ebdf2c16c3039bf3e97d78ed9e815c7
Canale0107 <canale0107@icloud.com>
2024-09-19 22:44:35 +0900

    refactor

--- src/data_acquisition/acquisition_manager.py
+++ src/data_acquisition/acquisition_manager.py
@@ -11,7 +11,7 @@ import cv2
 from src.config import DataAcquisitionConfig, CameraConfig, InfluxDBConfig
 from src.camera import CameraManager, Image, ImageMetaData
 from src.data_acquisition.file_manager import FilePathManager
-from src.writers import DataManager, CSVManager, InfluxDBManager
+from src.writers import DataWriter, DataManager, CSVManager, InfluxDBManager
 
 
 logger = logging.getLogger(__name__)
@@ -19,26 +19,48 @@ logger = logging.getLogger(__name__)
 
 class DataAcquisitionManager():
     def __init__(self, config: DataAcquisitionConfig, session_id: str,
-                camera_manager: CameraManager, filepath_manager: FilePathManager,
-                data_managers: List[DataManager], stop_event: threading.Event) -> None:
+                 camera_manager: CameraManager, filepath_manager: FilePathManager,
+                 data_managers: List[DataManager], stop_event: threading.Event) -> None:
         self.config = config
         self.session_id = session_id
         self.camera_manager = camera_manager
         self.filepath_manager = filepath_manager
         self.data_managers = data_managers
         self.stop_event = stop_event
-        self._running = False
 
-    def stop_acquisition(self) -> None:
-        self._running = False
+    def capture_frame(self) -> Image:
+        """ フレームをキャプチャして Image オブジェクトを返す """
+        ret, frame = self.camera_manager.cap.read()
+        if not ret:
+            raise RuntimeError("Failed to capture frame from camera.")
+        frame = cv2.flip(frame, 1)  # 画像の左右反転
+        timestamp = datetime.now(timezone.utc)
+        filepath = self.filepath_manager.get_filepath(timestamp)
+        meta_data = ImageMetaData(
+            session_id=self.session_id,
+            camera_index=self.camera_manager.config.camera_index,
+            timestamp=timestamp,
+            filepath=filepath
+        )
+        return Image(frame, meta_data)
+
+    def save_image(self, image: Image, writers: List[DataWriter]) -> None:
+        """ イメージを保存し、データを書き込む """
+        image.save(self.config.data_dirpath)
+        for writer in writers:
+            try:
+                writer.write_data(image.meta_data)
+            except Exception as e:
+                logger.error("Failed to write data: %s", e)
+
+    def handle_error(self, e: Exception) -> None:
+        """ エラーハンドリング """
+        logger.error("Error during acquisition: %s", e)
 
     def start_acquisition(self) -> None:
         """ データ取得の開始 """
-
-        image_save_dirpath = self.config.data_dirpath
-
         with ExitStack() as stack:
-            camera_manager = stack.enter_context(self.camera_manager)
+            stack.enter_context(self.camera_manager)
 
             writers = []
             for data_manager in self.data_managers:
@@ -52,75 +74,74 @@ class DataAcquisitionManager():
             next_frame_time = time.time() + sleep_time  # 最初のフレーム取得時間を設定
 
             try:
-                self._running = True
-                while self._running and not self.stop_event.is_set():
+                while not self.stop_event.is_set():
                     try:
-                        ret, frame = camera_manager.cap.read()
-                        if not ret:
-                            raise RuntimeError("Failed to capture frame from camera.")
-
-                        frame = cv2.flip(frame, 1)
-
-                        timestamp = datetime.now(timezone.utc)
-                        filepath = self.filepath_manager.get_filepath(timestamp)
-                        meta_data = ImageMetaData(
-                            session_id = self.session_id,
-                            camera_index = self.camera_manager.config.camera_index,
-                            timestamp = timestamp,
-                            filepath = filepath)
-
-                        image = Image(frame, meta_data)
-
-                        image.save(image_save_dirpath)
-                        for writer in writers:
-                            try:
-                                writer.write_data(meta_data)
-                            except Exception as e:
-                                logger.error("Failed to write data: %s", e)
+                        image = self.capture_frame()  # フレームをキャプチャ
+                        self.save_image(image, writers)  # イメージを保存
 
                         # 次のフレーム取得までの時間を計算し、必要ならスリープ
                         time.sleep(max(0, next_frame_time - time.time()))  # スリープ時間が負でないか確認
                         next_frame_time += sleep_time  # 次のフレーム取得時間を更新
 
                     except Exception as e:
-                        logger.error("Error capturing image: %s", e)
-                        raise
-
-            except Exception as e:
-                logger.error("Error during acquisition: %s", e)
-                self.stop_acquisition()
+                        self.handle_error(e)
+                        break  # エラー時にループを抜ける
 
             finally:
-                self.stop_acquisition()
+                logger.info("Acquisition stopped.")
 
 
-def run_acquisition_for_camera(camera_config: CameraConfig, data_acquisition_config: DataAcquisitionConfig, influxdb_config: InfluxDBConfig, session_id: str, stop_event: threading.Event) -> None:
-    """ カメラごとにデータ取得を行う """
-    csv_filepath = Path(data_acquisition_config.data_dirpath) / session_id / f'camera{camera_config.camera_index}' / 'meta_data.csv'
-
+def initialize_managers(camera_config, influxdb_config, session_id, csv_filepath):
     try:
         camera_manager = CameraManager(camera_config)
         csv_manager = CSVManager(csv_filepath)
         influxdb_manager = InfluxDBManager(influxdb_config, session_id)
         filepath_manager = FilePathManager(session_id, camera_config.camera_index)
-
+        return camera_manager, csv_manager, influxdb_manager, filepath_manager
     except Exception as e:
         logger.error("Initialization failed for camera %d: %s", camera_config.camera_index, e)
         raise
 
-    data_managers = []
 
+def get_data_managers(data_acquisition_config, csv_manager, influxdb_manager):
+    data_managers = []
     if data_acquisition_config.save_to_csv:
         data_managers.append(csv_manager)
-
     if data_acquisition_config.send_to_db:
         data_managers.append(influxdb_manager)
+    return data_managers
 
-    data_acquisition_manager = DataAcquisitionManager(data_acquisition_config, session_id, camera_manager, filepath_manager, data_managers, stop_event)
+
+def run_acquisition_for_camera(camera_config: CameraConfig,
+                               data_acquisition_config: DataAcquisitionConfig,
+                               influxdb_config: InfluxDBConfig,
+                               session_id: str, stop_event: threading.Event) -> None:
+    """ カメラごとにデータ取得を行う """
+
+    csv_filepath = (
+        Path(data_acquisition_config.data_dirpath)
+        / session_id
+        / f'camera{camera_config.camera_index}'
+        / 'meta_data.csv'
+    )
+
+    # マネージャーの初期化
+    camera_manager, csv_manager, influxdb_manager, filepath_manager = initialize_managers(
+        camera_config, influxdb_config, session_id, csv_filepath)
+
+    # データマネージャーの設定
+    data_managers = get_data_managers(data_acquisition_config, csv_manager, influxdb_manager)
+
+    data_acquisition_manager = DataAcquisitionManager(
+        data_acquisition_config,
+        session_id,
+        camera_manager,
+        filepath_manager,
+        data_managers, stop_event
+    )
 
     try:
         data_acquisition_manager.start_acquisition()
-
     except Exception as e:
         logger.error("Acquisition error for camera %d: %s", camera_config.camera_index, e)
         raise
